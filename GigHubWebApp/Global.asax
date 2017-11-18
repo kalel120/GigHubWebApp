db2123d782bc85122b6647e8b4d504b60a82fea4
Al Imran <imran_aust_cse@hotmail.com>
2017-11-18 20:31:22 +0600

    Initial

---
+++ GigHubWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GigHubWebApp.MvcApplication" Language="C#" %>
---
+++ GigHubWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GigHubWebApp.Startup))]
+namespace GigHubWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
