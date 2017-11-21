b8c62d5785af57752f7bc97895d6595e1a1cd9d7
Al Imran <imran_aust_cse@hotmail.com>
2017-11-22 00:00:18 +0600

    sln refactored

--- GigHubWebApp/Controllers/AccountController.cs
+++ GigHubWebApp/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using GigHubWebApp.Models;
+using GigHubWebApp.ViewModels;
 
 namespace GigHubWebApp.Controllers {
     [Authorize]
--- GigHubWebApp/Controllers/ManageController.cs
+++ GigHubWebApp/Controllers/ManageController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using GigHubWebApp.Models;
+using GigHubWebApp.ViewModels;
 
 namespace GigHubWebApp.Controllers
 {
