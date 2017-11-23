620dac1236431243a1f263c5f58ae55b8e684d09
mimran <mimran@bd.imshealth.com>
2017-11-23 14:15:41 +0600

    Attending any gig >> design done

---
+++ GigHubWebApp/Controllers/AttendancesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Web.Http;
+using GigHubWebApp.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHubWebApp.Controllers {
+    [Authorize]
+    public class AttendancesController : ApiController {
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public AttendancesController() {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult Attend([FromBody] int gigId) {
+            var userId = User.Identity.GetUserId();
+
+            if (_dbContext.Attendences.Any(a => a.GigId == gigId && a.AttendeeId == userId)) {
+                return BadRequest("Duplicate Attendance");
+            }
+
+            var attendence = new Attendence {
+                GigId = gigId,
+                AttendeeId = userId
+            };
+
+            _dbContext.Attendences.Add(attendence);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
