f5b4ed85bdff0727312f4e9039bec73c5d9b7a99
mimran <mimran@bd.imshealth.com>
2017-12-06 10:36:45 +0600

    Added >> gigs api and cancel gigs functionality

---
+++ GigHubWebApp/Controllers/Api/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Web.Http;
+using GigHubWebApp.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GigHubWebApp.Controllers.Api {
+    [Authorize]
+    public class GigsController : ApiController {
+        private readonly ApplicationDbContext _dbContext;
+
+        public GigsController() {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id) {
+            var userId = User.Identity.GetUserId();
+
+            var gig = _dbContext.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+            gig.IsCanceled = true;
+
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
