cf79e25ebd0c5f159e81e1d7da9d1deb70411e83
mimran <mimran@bd.imshealth.com>
2017-12-07 15:45:42 +0600

    Api: get all notifications

---
+++ GigHubWebApp/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,28 @@
+﻿using GigHubWebApp.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHubWebApp.Controllers.Api {
+    [Authorize]
+    public class NotificationsController : ApiController {
+        private readonly ApplicationDbContext _dbContext;
+
+        public NotificationsController() {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Notification> GetNewNotifications() {
+            var userId = User.Identity.GetUserId();
+            var notifications = _dbContext.UserNotifications
+                                            .Where(un => un.UserId == userId)
+                                            .Select(n => n.Notification)
+                                            .Include(n => n.Gig.Artist)
+                                            .ToList();
+
+            return notifications;
+        }
+    }
+}
