e351fd0a4da6c01e2a3c2d6dd361a8fb1cc41c4f
mimran <mimran@bd.imshealth.com>
2017-12-11 17:25:55 +0600

    Added >> animate.css : bounceInUp

--- GigHubWebApp/App_Start/BundleConfig.cs
+++ GigHubWebApp/App_Start/BundleConfig.cs
@@ -23,7 +23,8 @@ namespace GigHubWebApp {
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/animate.min.css"));
         }
     }
 }
