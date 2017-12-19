1fe66fabda19e75a48631c83a871f58fdfa4ec6f
kalel120 <mimran@bd.imshealth.com>
2017-12-19 15:13:05 +0600

    compressed bundles

--- GigHubWebApp/App_Start/BundleConfig.cs
+++ GigHubWebApp/App_Start/BundleConfig.cs
@@ -4,10 +4,13 @@ namespace GigHubWebApp {
     public class BundleConfig {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/underscore.min.js",
-                        "~/Scripts/moment.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js"
+                        , "~/Scripts/underscore.min.js"
+                        , "~/Scripts/moment.min.js"
+                        , "~/Scripts/bootstrap.js"
+                        , "~/Scripts/respond.js"
+                        , "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -16,11 +19,6 @@ namespace GigHubWebApp {
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
