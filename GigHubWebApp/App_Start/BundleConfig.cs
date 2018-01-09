503f3712a90efe59f184101ff79961e00b731d67
kalel120 <mimran@bd.imshealth.com>
2018-01-09 14:28:34 +0600

    js refactored >> Reavealing module pattern

--- GigHubWebApp/App_Start/BundleConfig.cs
+++ GigHubWebApp/App_Start/BundleConfig.cs
@@ -4,6 +4,10 @@ namespace GigHubWebApp {
     public class BundleConfig {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app/app.js"
+                ));
+
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js"
                         , "~/Scripts/underscore.min.js"
