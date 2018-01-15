bf65999b2b88e2b4dcc3131b0390da88012c969c
kalel120 <imran_aust_cse@hotmail.com>
2018-01-15 14:41:22 +0600

    Refactored HomeController

--- GigHubWebApp/Controllers/AccountController.cs
+++ GigHubWebApp/Controllers/AccountController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
-using GigHubWebApp.Core.Models;
+﻿using GigHubWebApp.Core.Models;
 using GigHubWebApp.Core.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace GigHubWebApp.Controllers {
     [Authorize]
@@ -99,9 +96,9 @@ namespace GigHubWebApp.Controllers {
                 return View(model);
             }
 
-            // The following code protects for brute force attacks against the two factor codes. 
-            // If a user enters incorrect codes for a specified amount of time then the user account 
-            // will be locked out for a specified amount of time. 
+            // The following code protects for brute force attacks against the two factor codes.
+            // If a user enters incorrect codes for a specified amount of time then the user account
+            // will be locked out for a specified amount of time.
             // You can configure the account lockout settings in IdentityConfig
             var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result) {
@@ -188,7 +185,7 @@ namespace GigHubWebApp.Controllers {
                 // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                 // Send an email with this link
                 // string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
+                // var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                 // await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
                 // return RedirectToAction("ForgotPasswordConfirmation", "Account");
             }
--- GigHubWebApp/Controllers/HomeController.cs
+++ GigHubWebApp/Controllers/HomeController.cs
@@ -1,39 +1,30 @@
-﻿using GigHubWebApp.Core.ViewModels;
-using GigHubWebApp.Persistence;
-using GigHubWebApp.Repositories;
+﻿using GigHubWebApp.Core;
+using GigHubWebApp.Core.ViewModels;
 using Microsoft.AspNet.Identity;
 using System.Linq;
 using System.Web.Mvc;
 
 namespace GigHubWebApp.Controllers {
     public class HomeController : Controller {
-        private readonly ApplicationDbContext _dbContext;
-        private readonly AttendancesRepository _attendancesRepo;
-        private readonly FollowingRepository _followingRepo;
-        private readonly GigsRepositories _gigsRepo;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController() {
-            _dbContext = new ApplicationDbContext();
-            _attendancesRepo = new AttendancesRepository(_dbContext);
-            _followingRepo = new FollowingRepository(_dbContext);
-            _gigsRepo = new GigsRepositories(_dbContext);
+        public HomeController(IUnitOfWork unitOfWork) {
+            _unitOfWork = unitOfWork;
         }
 
-
         public ActionResult Index(string query = null) {
-            var userId = User.Identity.GetUserId();
-
-            var upcomingGigs = _gigsRepo.GetAllUpcomingGigs();
+            var upcomingGigs = _unitOfWork.GigsRepo.GetAllUpcomingGigs();
 
             if (!string.IsNullOrWhiteSpace(query)) {
-                upcomingGigs = _gigsRepo.GetUpcomingGigsSearchResult(query, upcomingGigs);
+                upcomingGigs = _unitOfWork.GigsRepo.GetUpcomingGigsSearchResult(query, upcomingGigs);
             }
 
+            var userId = User.Identity.GetUserId();
             var upcomingGigsViewModel = new GigsViewModel {
                 UpcomingGigs = upcomingGigs,
                 IsAuthenticated = User.Identity.IsAuthenticated,
-                Followees = _followingRepo.GetFollowingsTableByFollowerId(userId).ToLookup(f => f.FolloweeId),
-                Attendences = _attendancesRepo.GetUsersFutureAttendances(userId).ToLookup(a => a.GigId),
+                Followees = _unitOfWork.FollowingRepo.GetFollowingsTableByFollowerId(userId).ToLookup(f => f.FolloweeId),
+                Attendences = _unitOfWork.AttendancesRepo.GetUsersFutureAttendances(userId).ToLookup(a => a.GigId),
                 Heading = "All Upcoming Gigs",
                 SearchTerm = query
             };
