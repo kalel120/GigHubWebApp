da3d37b205b0c97231e1d900675c2db4bfbc0ab4
kalel120 <imran_aust_cse@hotmail.com>
2018-01-23 12:32:17 +0600

    refactored mock user class

--- GigHubWebApp.Tests/Controllers/Api/GigsControllerUnitTests.cs
+++ GigHubWebApp.Tests/Controllers/Api/GigsControllerUnitTests.cs
@@ -1,9 +1,8 @@
 ﻿using GigHubWebApp.Controllers.Api;
 using GigHubWebApp.Core;
+using GigHubWebApp.Tests.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using System.Security.Claims;
-using System.Security.Principal;
 
 
 namespace GigHubWebApp.Tests.Controllers.Api {
@@ -12,15 +11,9 @@ namespace GigHubWebApp.Tests.Controllers.Api {
         private GigsController _gigsController;
 
         public GigsControllerUnitTests() {
-            var identity = new GenericIdentity("user1@domain.com");
-            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", "user1@domain.com"));
-            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", "1"));
-
-            var principal = new GenericPrincipal(identity, null);
-
             var mockUoW = new Mock<IUnitOfWork>();
             _gigsController = new GigsController(mockUoW.Object);
-            _gigsController.User = principal;
+            _gigsController.MockThisUser("1", "user1@domain.com");
 
         }
 
---
+++ GigHubWebApp.Tests/Extensions/ApiControllerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using System.Web.Http;
+
+namespace GigHubWebApp.Tests.Extensions {
+    public static class ApiControllerExtensions {
+        public static void MockThisUser(this ApiController controller, string userid, string username) {
+            var identity = new GenericIdentity(username);
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", username));
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", userid));
+
+            var principal = new GenericPrincipal(identity, null);
+            controller.User = principal;
+        }
+    }
+}
